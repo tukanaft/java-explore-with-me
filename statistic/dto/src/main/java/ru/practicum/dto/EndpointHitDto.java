a7dd4d573556d9f93c85b87f8563c2e1fe5581d9
tukanaft <tukanaft@gmail.com>
2025-01-13 21:11:48 +0300

    pom fix

--- statistic/dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ statistic/dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,11 +7,18 @@ import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class EndpointHitDto {
     Integer id;
     String app;
     String uri;
     String ip;
     LocalDateTime timestamp;
+
+    public EndpointHitDto(Integer id, String app, String uri, String ip, LocalDateTime timestamp) {
+        this.id = id;
+        this.app = app;
+        this.uri = uri;
+        this.ip = ip;
+        this.timestamp = timestamp;
+    }
 }
